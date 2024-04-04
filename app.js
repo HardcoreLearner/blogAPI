8b191ad0fc51e10370a802465a09007f7573b1fa
HardcoreLearner <MoBah@protonmail.com>
2024-04-04 16:07:36 +0000

    Romoved view also from app

--- app.js
+++ app.js
@@ -32,10 +32,6 @@ async function main() {
   await mongoose.connect(mongoDB);
 }
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
